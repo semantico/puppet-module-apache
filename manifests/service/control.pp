97b813d8de2b00e0ab68327a650328edf502b707
Mike Pountney <Mike.Pountney@gmail.com>
2012-10-25 12:36:01 +0100

    try full namespace var loading

--- manifests/service/control.pp
+++ manifests/service/control.pp
@@ -1,23 +1,26 @@
 
-class apache::service::control inherits apache::config {
+class apache::service::control {
+
+        include apache::config
 
         exec { "reload-apache2":
-                command => "/etc/init.d/${apache2_service_name} reload",
+                command => "/etc/init.d/${apache::config::apache2_service_name} reload",
                 refreshonly => true,
                 before => [ Service["apache2"], Exec["force-reload-apache2"] ],
         }
 
         exec { "force-reload-apache2":
-                command => "/etc/init.d/${apache2_service_name} force-reload",
+                command => "/etc/init.d/${apache::config::apache2_service_name} force-reload",
                 refreshonly => true,
                 before => Service["apache2"],
         }
 
 
         exec { "restart-apache2":
-                command => "/etc/init.d/${apache2_service_name} restart",
+                command => "/etc/init.d/${apache::config::apache2_service_name} restart",
                 refreshonly => true,
                 before => Service["apache2"],
         }
+
 }
 
